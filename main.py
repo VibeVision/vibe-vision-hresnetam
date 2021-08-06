0de008fada51b3850d2c7a68d439da620a0558c6
VibeVision <VibeVisio80@proton.me>
2021-08-06 08:14:48 +0000

    coding style cleanup

--- main.py
+++ main.py
@@ -30,3 +30,30 @@ ensemble = 1
 
 day = datetime.datetime.now()
 day_str = day.strftime('%m_%d_%H_%M')
+
+print('-----Importing Dataset-----')
+global Dataset
+dataset = 'PC'
+Dataset = dataset.upper()
+data_hsi, gt_hsi, TOTAL_SIZE, TRAIN_SIZE,VALIDATION_SPLIT = load_dataset(Dataset)
+print(data_hsi.shape)
+
+ROWS, COLUMNS, BAND = data_hsi.shape
+data = data_hsi.reshape(np.prod(data_hsi.shape[:2]), np.prod(data_hsi.shape[2:]))
+gt = gt_hsi.reshape(np.prod(gt_hsi.shape[:2]),)
+CLASSES_NUM = int(max(gt))
+print('The class numbers of the HSI data is:', CLASSES_NUM)
+
+print('-----Importing Setting Parameters-----')
+ITER = 2
+PATCH_LENGTH = 3
+lr, num_epochs, batch_size = 0.0002, 300, 32
+loss = torch.nn.CrossEntropyLoss()
+
+img_rows = 2*PATCH_LENGTH+1
+img_cols = 2*PATCH_LENGTH+1
+INPUT_DIMENSION = data_hsi.shape[2]
+ALL_SIZE = data_hsi.shape[0] * data_hsi.shape[1]
+VAL_SIZE = int(TRAIN_SIZE)
+TEST_SIZE = TOTAL_SIZE - TRAIN_SIZE
+SAMPLES_NUM = 200
