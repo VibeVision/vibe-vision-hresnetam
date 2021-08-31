d66238db24e8dcaa7b7dda2ba79f5994adfe5c4c
VibeVision <VibeVisio80@proton.me>
2021-08-31 15:58:44 +0000

    Version 1.8

--- networks/network.py
+++ networks/network.py
@@ -6,3 +6,34 @@ Created on Wed Oct 21 21:08:41 2020
 @author: xuegeeker
 @email: xuegeeker@163.com
 """
+
+import torch
+from torch import nn
+import math
+import numpy as np
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.nn import Module, Sequential, Conv2d, Conv3d, ReLU,AdaptiveMaxPool2d, AdaptiveAvgPool2d, \
+    NLLLoss, BCELoss, CrossEntropyLoss, AvgPool2d, MaxPool2d, Parameter, Linear, Sigmoid, Softmax, Dropout, Embedding
+
+
+class mish(nn.Module):
+    def __init__(self):
+        super(mish, self).__init__()
+    def forward(self, x):
+        return x * torch.tanh(F.softplus(x))
+
+
+class PAM_Module(Module):
+    def __init__(self, in_dim):
+        super(PAM_Module, self).__init__()
+        self.chanel_in = in_dim
+        self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax = Softmax(dim=-1)
+        
+    def forward(self, x):
+        x = x.squeeze(-1)
+        m_batchsize, C, height, width = x.size()
