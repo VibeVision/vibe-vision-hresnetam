a814f20132c2d571f1e2dfcb733ec01b6a8e298d
VibeVision <VibeVisio80@proton.me>
2021-09-01 10:01:45 +0000

    upgrade rails

--- networks/network.py
+++ networks/network.py
@@ -37,3 +37,32 @@ class PAM_Module(Module):
     def forward(self, x):
         x = x.squeeze(-1)
         m_batchsize, C, height, width = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width * height)
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+        out = (self.gamma*out + x).unsqueeze(-1)
+        return out
+
+
+class CAM_Module(Module):
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax  = Softmax(dim=-1)
+        
+    def forward(self, x):
+        m_batchsize, C, height, width, channle = x.size()
+        proj_query = x.view(m_batchsize, C, -1)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+        proj_value = x.view(m_batchsize, C, -1)
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width, channle)
+        out = self.gamma*out + x 
