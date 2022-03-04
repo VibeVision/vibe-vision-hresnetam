5291ebc5df94350af0290e4f6d4eb3422af6302c
VibeVision <VibeVisio80@proton.me>
2022-03-04 22:48:53 +0000

    removed debugger statement

--- utils/extract_samll_cubic.py
+++ utils/extract_samll_cubic.py
@@ -23,4 +23,4 @@ def assignment_index(assign_0, assign_1, col):
 
 
 def select_patch(matrix, pos_row, pos_col, ex_len):
-    selected_rows = 
+    selected_rows = matrix[range(pos_row-ex_len, pos_row+e
