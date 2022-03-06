5ded9c5857a1f0254900910111c5b7b8e70d5f85
VibeVision <VibeVisio80@proton.me>
2022-03-06 07:07:04 +0000

    version tag

--- utils/extract_samll_cubic.py
+++ utils/extract_samll_cubic.py
@@ -23,4 +23,5 @@ def assignment_index(assign_0, assign_1, col):
 
 
 def select_patch(matrix, pos_row, pos_col, ex_len):
-    selected_rows = matrix[range(pos_row-ex_len, pos_row+e
+    selected_rows = matrix[range(pos_row-ex_len, pos_row+ex_len+1)]
+    selected_p
