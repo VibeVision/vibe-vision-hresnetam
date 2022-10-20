6648f6eb30925255d02b5abd7089ced85dfd2e52
VibeVision <VibeVisio80@proton.me>
2022-10-20 20:17:24 +0000

    forget

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -116,4 +116,4 @@ def sampling2(ground_truth, proportion):
 def aa_and_each_accuracy(confusion_matrix):
     list_diag = np.diag(confusion_matrix)
     list_raw_sum = np.sum(confusion_matrix, axis=1)
-    each_acc
+    each_acc = np.nan_to_num(true
