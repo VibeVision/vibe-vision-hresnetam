1867354d0efdd2a41c97d60c749f6e9780ae0eb6
VibeVision <VibeVisio80@proton.me>
2022-11-12 13:00:52 +0000

    update content

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -139,4 +139,5 @@ def classification_map(map, ground_truth, dpi, save_path):
 
 def list_to_colormap(x_list):
     y = np.zeros((x_list.shape[0], 3))
-    for index, item in enumerate(x_
+    for index, item in enumerate(x_list):
+        if item ==
