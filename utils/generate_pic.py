2a6a97ba02c0a99ea60263ce5df886257d903f1b
VibeVision <VibeVisio80@proton.me>
2022-11-23 19:47:39 +0000

    Swiftlinting

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -145,4 +145,5 @@ def list_to_colormap(x_list):
         if item == 1:
             y[index] = np.array([255, 0, 0]) / 255.
         if item == 2:
-            y[index] = np.array([100, 255, 1
+            y[index] = np.array([100, 255, 100])/255.
+        if item 
