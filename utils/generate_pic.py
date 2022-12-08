ee33c4a91196a58d84283af5a769b99a38711830
VibeVision <VibeVisio80@proton.me>
2022-12-08 08:37:47 +0000

    Delete isup.lua

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -153,4 +153,5 @@ def list_to_colormap(x_list):
         if item == 5:
             y[index] = np.array([255, 0, 255])/255.
         if item == 6:
-            y[index] = np.array([255, 100, 100])/
+            y[index] = np.array([255, 100, 100])/255.
+        if item 
