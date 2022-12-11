57c837fd015148339009b07718b67a80d36e0294
VibeVision <VibeVisio80@proton.me>
2022-12-11 06:50:41 +0000

    package renaming

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -155,4 +155,4 @@ def list_to_colormap(x_list):
         if item == 6:
             y[index] = np.array([255, 100, 100])/255.
         if item == 7:
-            y[in
+            y[index] = np.array([1
