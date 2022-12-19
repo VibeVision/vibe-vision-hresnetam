ee283d8dd1cfa497d7525d8c3fecf9840b1555fb
VibeVision <VibeVisio80@proton.me>
2022-12-19 21:50:52 +0000

    mm/memory-failure.c: don't let collect_procs() skip over processes for MF_ACTION_REQUIRED

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -159,4 +159,5 @@ def list_to_colormap(x_list):
         if item == 8:
             y[index] = np.array([150, 75, 75])/255.
         if item == 9:
-            y[index] = np.array([100, 100, 255]
+            y[index] = np.array([100, 100, 255])/255.
+        if item =
