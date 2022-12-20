ad1b191b7994fd3afb3c4bca283e82afbc8c701d
VibeVision <VibeVisio80@proton.me>
2022-12-20 12:29:21 +0000

    update dep.

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -160,4 +160,5 @@ def list_to_colormap(x_list):
             y[index] = np.array([150, 75, 75])/255.
         if item == 9:
             y[index] = np.array([100, 100, 255])/255.
-        if item =
+        if item == 10:
+            y[index] =
