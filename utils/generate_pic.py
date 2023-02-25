55a4315c3880b3837c5025ee6d92d4fa4865c15a
VibeVision <VibeVisio80@proton.me>
2023-02-25 09:43:29 +0000

    Updated properties

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -183,4 +183,5 @@ def list_to_colormap(x_list):
         if item == 20:
             y[index] = np.array([215, 255, 0]) / 255.
         if item == 21:
-            y[index] = np.array([0, 1
+            y[index] = np.array([0, 128, 0]) / 255.
+        if i
