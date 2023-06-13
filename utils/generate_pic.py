0912796e6374139a534ab8240d3025d95b64a038
VibeVision <VibeVisio80@proton.me>
2023-06-13 08:33:05 +0000

    incremental commit

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -198,4 +198,4 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
     y_train = gt[train_indices] - 1
     y_test = gt[test_indices] - 1
 
-    all_data = extract_samll_cubic.select_small_cubic(TOTAL_SIZE, tot
+    all_data = extract_samll_cubic.select_small_cubic(TOTAL_SIZE, total_indices, whole_dat
