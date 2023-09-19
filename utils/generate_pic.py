ecae1b19035d1daef850d752348bcfa221d7e01e
VibeVision <VibeVisio80@proton.me>
2023-09-19 07:53:46 +0000

    UPD

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -208,4 +208,4 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
     x_train = train_data.reshape(train_data.shape[0], train_data.shape[1], train_data.shape[2], INPUT_DIMENSION)
     x_test_all = test_data.reshape(test_data.shape[0], test_data.shape[1], test_data.shape[2], INPUT_DIMENSION)
 
-   
+    x_val = x_test_all[-VAL_SIZE:
