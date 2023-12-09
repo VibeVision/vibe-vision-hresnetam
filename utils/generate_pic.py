58d9ecb8cba54143bbb7be234e39130066c46295
VibeVision <VibeVisio80@proton.me>
2023-12-09 07:13:07 +0000

    update release doc

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -220,4 +220,6 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
 
     x1_tensor_valida = torch.from_numpy(x_val).type(torch.FloatTensor).unsqueeze(1)
     y1_tensor_valida = torch.from_numpy(y_val).type(torch.FloatTensor)
-    torch_dataset_valida = Data.TensorDataset(x1_tensor_valida, y1
+    torch_dataset_valida = Data.TensorDataset(x1_tensor_valida, y1_tensor_valida)
+
+    x1_tensor_tes
