d28ffb3227bc7eb87e8ab45e363c0b30d902fc20
VibeVision <VibeVisio80@proton.me>
2023-12-19 22:21:26 +0000

    Addressing code review comments

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -233,4 +233,5 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
 
 
     train_iter = Data.DataLoader(
-        dataset=torch_datas
+        dataset=torch_dataset_train,
+        batch_si
