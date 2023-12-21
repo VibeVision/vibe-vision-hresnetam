15b2dfeb59d70ce99036a1891bad7be941600b69
VibeVision <VibeVisio80@proton.me>
2023-12-21 18:43:26 +0000

    Update bits-oder-functions.js

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -240,4 +240,5 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
     )
     valiada_iter = Data.DataLoader(
         dataset=torch_dataset_valida,
-        batch
+        batch_size=batch_size,
+ 
