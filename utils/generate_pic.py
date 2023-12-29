a1ff6508a4af06eb8f71b8dcc1a49a9440a3313f
VibeVision <VibeVisio80@proton.me>
2023-12-29 12:15:31 +0000

    Added setup.py.

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -244,4 +244,5 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
         shuffle=True,
         num_workers=0,
     )
-    test_iter = Data.Data
+    test_iter = Data.DataLoader(
+        dataset=torch
