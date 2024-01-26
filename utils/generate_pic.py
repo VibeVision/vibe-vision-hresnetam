f9cd282730d25bbd663a908adfc203f27e1b7192
VibeVision <VibeVisio80@proton.me>
2024-01-26 10:07:54 +0000

    Added demo gif

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -253,4 +253,5 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
     all_iter = Data.DataLoader(
         dataset=torch_dataset_all,
         batch_size=batch_size,
-       
+        shuffle=False,
+        num_w
