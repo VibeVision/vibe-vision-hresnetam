a19851d0a3b0a137888a6287ef4dbe67fc6e8f36
VibeVision <VibeVisio80@proton.me>
2024-02-15 14:25:44 +0000

    zswap: use LZ4 if available

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -256,4 +256,4 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
         shuffle=False,
         num_workers=0,
     )
-    return tr
+    return train_iter, valiada_iter, test_it
