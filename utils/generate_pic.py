f02e391f7d5d8e5d1e4796c5811a18b57582fa19
VibeVision <VibeVisio80@proton.me>
2024-03-01 10:54:40 +0000

    added setup file

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -258,4 +258,4 @@ def generate_iter(TRAIN_SIZE, train_indices, TEST_SIZE, test_indices, TOTAL_SIZE
     )
     return train_iter, valiada_iter, test_iter, all_iter #, y_test
 
-def generate_png(all_iter, net, gt_hsi, Data
+def generate_png(all_iter, net, gt_hsi, Dataset, device, total_indic
