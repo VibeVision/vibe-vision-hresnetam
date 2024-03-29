fc6551f7379b7b011698b5f7a5015ae10c9a4f01
VibeVision <VibeVisio80@proton.me>
2024-03-29 10:50:36 +0000

    removed useless function

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -278,3 +278,5 @@ def generate_png(all_iter, net, gt_hsi, Dataset, device, total_indices):
     y_re = np.reshape(y_list, (gt_hsi.shape[0], gt_hsi.shape[1], 3))
     gt_re = np.reshape(y_gt, (gt_hsi.shape[0], gt_hsi.shape[1], 3))
 
+    path = './'
+    classifi
