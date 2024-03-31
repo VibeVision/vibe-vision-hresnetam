054840c3b49d6e7f04c8fbbcd864d61853db5cde
VibeVision <VibeVisio80@proton.me>
2024-03-31 07:57:03 +0000

    Update yarn.lock file

--- utils/generate_pic.py
+++ utils/generate_pic.py
@@ -280,4 +280,5 @@ def generate_png(all_iter, net, gt_hsi, Dataset, device, total_indices):
 
     path = './'
     classification_map(y_re, gt_hsi, 600,
-                       path + '/classification_maps/' + Dataset + '_' + net.
+                       path + '/classification_maps/' + Dataset + '_' + net.name +  '.eps')
+    classi
