aa0d513a0412a0f9261734906f3bf66c120f0f96
VibeVision <VibeVisio80@proton.me>
2024-05-30 07:31:39 +0000

    Add github actions

--- utils/record.py
+++ utils/record.py
@@ -29,4 +29,4 @@ def record_output(oa_ae, aa_ae, kappa_ae, element_acc_ae, training_time_ae, test
     f.write(sentence7)
 
     element_mean = np.mean(element_acc_ae, axis=0)
-    element_std = n
+    element_std = np.std(element_acc_ae, axis=0
